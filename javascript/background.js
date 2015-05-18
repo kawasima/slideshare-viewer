72cf1a760be92ef9408a70671be38aca5a78b014
kawasima <kawasima1016@gmail.com>
2015-05-18 19:49:51 +0900

    Change declarativeWebRequest to webRequest.

--- javascript/background.js
+++ javascript/background.js
@@ -1,50 +1,38 @@
-var ruleSpeakerdeck = {
-    conditions: [
-        new chrome.declarativeWebRequest.RequestMatcher({
-            url: { hostSuffix: 'speakerdeck.com'},
-            stages: ['onBeforeRequest']
-        })
-    ],
-    actions: [
-        new chrome.declarativeWebRequest.SendMessageToExtension({message: "speakerdeck"}),
-        new chrome.declarativeWebRequest.CancelRequest()
-    ]
-};
-
-var ruleSlideshare = {
-    conditions: [
-        new chrome.declarativeWebRequest.RequestMatcher({
-            url: { hostSuffix: 'slideshare.net'},
-            stages: ['onBeforeRequest']
-        })
-    ],
-    actions: [
-        new chrome.declarativeWebRequest.SendMessageToExtension({message: "slideshare"}),
-        new chrome.declarativeWebRequest.CancelRequest()
-    ]
-};
-
 var loading = false;
-chrome.declarativeWebRequest.onRequest.addRules([ruleSpeakerdeck, ruleSlideshare]);
-chrome.declarativeWebRequest.onMessage.addListener(function(response) {
+function dispatch(site, url, tabId) {
     if (loading) return false;
     loading = true;
-    var protocol = response.url.match("^(http|https)://");
-    var url = "http://sssslide.com/" + response.url.substr(protocol[0].length);
+    var protocol = url.match("^(http|https)://");
+    var sssslideUrl = "http://sssslide.com/" + url.substr(protocol[0].length);
 
     setTimeout(function() { loading = false; }, 5000);
-    $.get(url, function(data) {
-        loading = false;
+    $.get(sssslideUrl, function(data) {
         var metadata = $(data).filter("#js__slide");
 
         chrome.storage.local.set({
             total: parseInt(metadata.attr("data-total-slides")),
-            type: response.message,
+            type: site,
             baseUrl: metadata.attr("data-image-url-template"),
             title: $(data).filter("meta[property='og:title']").attr("content")
         }, function() {
-            chrome.tabs.update(response.tabId, {url: chrome.extension.getURL("/slide.html")});
+            chrome.tabs.update(tabId, {url: chrome.extension.getURL("/slide.html")}, function() {
+                if (chrome.runtime.lastError) {
+                    console.log(chrome.runtime.lastError.message);
+                }
+            });
         });
     });
-});
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+        function(request) {
+            dispatch("speakerdeck", request.url, request.tabId); 
+        },
+        {urls: ["*://speakerdeck.com/*"]});
+
+chrome.webRequest.onBeforeRequest.addListener(
+        function(request) {
+            dispatch("slideshare", request.url, request.tabId); 
+        },
+        {urls: ["*://www.slideshare.net/*"]});
 
