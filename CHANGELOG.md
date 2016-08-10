1289ca3cb0a932e061ba69a523c445dbe6d12873
kawasima <kawasima1016@gmail.com>
2016-08-10 14:46:08 +0900

    Added a new transition method.

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 0.5.0 (2016-08-10)
+
+* Add a new slide transition method by clicking a image left-side or right-side.
+* Jump the specific page by clicking the progress bar.
--- javascript/background.js
+++ javascript/background.js
@@ -1,5 +1,8 @@
 var loading = false;
+var pluginActive = false;
+
 function dispatch(site, url, tabId) {
+    if (pluginActive) return false;
     if (loading) return false;
     loading = true;
     var protocol = url.match("^(http|https)://");
@@ -25,14 +28,19 @@ function dispatch(site, url, tabId) {
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
-        function(request) {
-            dispatch("speakerdeck", request.url, request.tabId); 
-        },
-        {urls: ["*://speakerdeck.com/*"]});
+    (request) => dispatch("speakerdeck", request.url, request.tabId),
+    {
+        urls: ["*://speakerdeck.com/*"]
+    });
 
 chrome.webRequest.onBeforeRequest.addListener(
-        function(request) {
-            dispatch("slideshare", request.url, request.tabId); 
-        },
-        {urls: ["*://www.slideshare.net/*"]});
+    (request) => dispatch("slideshare", request.url, request.tabId),
+    {
+        urls: ["*://www.slideshare.net/*"]
+    });
 
+$.ajax("http://www.slideshare.net/favicon.ico", {
+    error: (xhr, status, ex) => {
+        pluginActive = true;
+    }
+});
